4e2733c91a5c400cde639666e8c0cc5297768158
Gilherme <guibarreto64042@gmail.com>
2024-07-29 15:37:30 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import styles from './home.module.css'
+
+function Home () {
+    return (
+        <>
+          <main className={styles.home}>
+              <h1>Home</h1>
+          </main>
+        </>
+    );
+}
+
+export default Home;
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home'
+import PageNotFound from './pages/PageNotFound'
+
+function AppRoutes () {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <Home /> }></Route>
+                <Route path="/ol" element={ <PageNotFound /> }></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default AppRoutes;
